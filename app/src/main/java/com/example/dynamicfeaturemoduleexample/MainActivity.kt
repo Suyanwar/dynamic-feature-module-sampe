ec861c1055df5f88ca60f3274527b9c496422169
Suyanwar <Suyanwar.wawang@gmail.com>
2022-07-13 10:28:13 +0700

    feat(DFM-Example): Implement view binding and add cta for 4 types of DFM

--- app/src/main/java/com/example/dynamicfeaturemoduleexample/MainActivity.kt
+++ app/src/main/java/com/example/dynamicfeaturemoduleexample/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.dynamicfeaturemoduleexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.dynamicfeaturemoduleexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.run {
+            acbInstallTimeDelivery.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Soon to be develop", Toast.LENGTH_SHORT).show()
+            }
+            acbOnDemandDelivery.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Soon to be develop", Toast.LENGTH_SHORT).show()
+            }
+            acbConditionalDelivery.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Soon to be develop", Toast.LENGTH_SHORT).show()
+            }
+            acbInstantDelivery.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Soon to be develop", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
